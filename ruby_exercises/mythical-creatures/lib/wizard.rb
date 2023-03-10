b922db4615e111c1864e21f9d6b2994cf537b851
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-10 13:54:48 -0700

    Add wizard and modify wizard spec to pass

---
+++ ruby_exercises/mythical-creatures/lib/wizard.rb
@@ -0,0 +1,29 @@
+class Wizard
+  attr_reader :name, :bearded, :rested
+
+  def initialize(name, bearded: true)
+    @name = name
+    @bearded = bearded
+    @rested = true
+    @spells_cast = 0
+  end
+
+  def bearded?
+    @bearded
+  end
+
+  def incantation(message)
+    "sudo #{message}"
+  end
+
+  def rested?
+    return false if @spells_cast >= 3
+    rested
+   end
+  
+  def cast
+    @spells_cast += 1
+    "MAGIC MISSILE!"
+  end
+
+end
