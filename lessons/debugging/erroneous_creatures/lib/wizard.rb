1d794102bcd244a87ce48d80be64fccabeb8a49d
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-24 09:52:17 -0600

    Fix wizard erroneous errors

--- lessons/debugging/erroneous_creatures/lib/wizard.rb
+++ lessons/debugging/erroneous_creatures/lib/wizard.rb
@@ -3,13 +3,18 @@ class Wizard
   attr_reader :name
   attr_accessor :bearded
 
-  def initialize(name, bearded = {bearded: true})
+  def initialize(name, bearded: true)
     @name = name
     @bearded = bearded
   end
 
   def bearded?
     @bearded
+    # if @bearded == true
+    #   true
+    # else
+    #   false
+    # end
   end
 
   def incantation(x)
