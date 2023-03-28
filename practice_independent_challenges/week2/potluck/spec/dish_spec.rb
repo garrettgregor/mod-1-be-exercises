b7339227dea95a70c22e92a71c099fb6b0359ed7
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-28 14:58:44 -0600

    Finish iteration 1 of ICS practice week 2

---
+++ practice_independent_challenges/week2/potluck/spec/dish_spec.rb
@@ -0,0 +1,12 @@
+require "rspec"
+require "./lib/dish"
+
+RSpec.describe Dish do
+  it "exists" do
+    dish = Dish.new("Couscous Salad", :appetizer)
+
+    expect(dish).to be_an_instance_of(Dish)
+    expect(dish.name).to eq("Couscous Salad")
+    expect(dish.category).to eq(:appetizer)
+  end
+end
