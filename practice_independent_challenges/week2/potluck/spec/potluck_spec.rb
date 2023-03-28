421a6ab52073f369e3d5163cb2e8e6a08a6bbbce
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-28 15:24:38 -0600

    Verify how many dishes there are and remove redundant test from add dish test

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -23,9 +23,21 @@ RSpec.describe Potluck do
 
     potluck.add_dish(couscous_salad)
     potluck.add_dish(cocktail_meatballs)
-    potluck.dishes
     
     expect(potluck.date).to eq("7-13-18")
     expect(potluck.dishes).to eq(["Couscous Salad", "Cocktail Meatballs"])
+  end
+  
+  it "verifies how many dishes there are" do
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+    
+    expect(potluck.date).to eq("7-13-18")
+    expect(potluck.dishes).to eq(["Couscous Salad", "Cocktail Meatballs"])
+    expect(potluck.dishes.length).to eq(2)
   end
 end
