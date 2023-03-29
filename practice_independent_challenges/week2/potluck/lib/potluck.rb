22407e91651708f89a423012d3d8d0bc4103d57e
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-28 18:47:49 -0600

    Finish iteration 3 and fix errors with testing in iteration 2

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -1,13 +1,45 @@
 class Potluck
   attr_reader :date,
               :dishes
+              # :dish_arrays
   def initialize(date)
     @date = date
     @dishes = []
+    # @dish_arrays = []
   end
 
   def add_dish(dish)
-    self.dishes << dish.name
+    # self.dish_arrays << [dish.name, dish.category]
+    # self.dishes << dish.name
+    dishes << dish
   end
 
+  def get_all_from_category(category)
+    dishes.select do |dish|
+      dish.category == category #if this statement is true, select will select the dish and put it into an array, otherwise it won't do anything
+    end
+  end
+  
+  # def get_all_from_category(category)
+  #   dish_arrays.each do |dish_array|
+  #     treanform = dish_array.sort
+  #     require 'pry'; binding.pry
+  #     sorted = []
+  #     sorted << dish_array
+  #     sorted.each do |category, name|
+
+  # end
+  
+  # def make_single_dish_array(dish)
+  #   [dish.name, dish.category]
+  # end
+
+  # def make_all_dishes_array(dish)
+  #   all_dishes = []
+  #   return_value = dishes.each do |dish|
+  #     dish.make_all_dishes_array(dish)
+  #   end
+  #   all_dishes << return_value
+  #   all_dishes
+  # end
 end
--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -25,19 +25,25 @@ RSpec.describe Potluck do
     potluck.add_dish(cocktail_meatballs)
     
     expect(potluck.date).to eq("7-13-18")
-    expect(potluck.dishes).to eq(["Couscous Salad", "Cocktail Meatballs"])
+    expect(potluck.dishes).to eq([couscous_salad, cocktail_meatballs])
+    expect(potluck.dishes.length).to eq(2)
   end
   
-  it "verifies how many dishes there are" do
+  it "can select specific categories of dishes" do
     potluck = Potluck.new("7-13-18")
     couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
     cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
-    
+    candy_salad = Dish.new("Candy Salad", :dessert)
     potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
     potluck.add_dish(cocktail_meatballs)
-    
-    expect(potluck.date).to eq("7-13-18")
-    expect(potluck.dishes).to eq(["Couscous Salad", "Cocktail Meatballs"])
-    expect(potluck.dishes.length).to eq(2)
+    potluck.add_dish(candy_salad)
+
+    expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
+    expect(potluck.get_all_from_category(:appetizer).first).to eq(couscous_salad)
+    expect(potluck.get_all_from_category(:appetizer).first.name).to eq("Couscous Salad")
   end
 end
