acc053b2b61f770ee9ea470a23985a92594bf318
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-30 14:32:03 -0600

    Fix medusa erroneous errors

--- lessons/debugging/erroneous_creatures/spec/medusa_spec.rb
+++ lessons/debugging/erroneous_creatures/spec/medusa_spec.rb
@@ -22,7 +22,8 @@ RSpec.describe Medusa do
   it "when first created she has no statues" do
     medusa = Medusa.new("Cassiopeia")
 
-    expect(medusa.statues.empty?).to eq([])
+    expect(medusa.statues).to eq([])
+    expect(medusa.statues.empty?).to eq(true)
   end
 
   it "when staring at a person she gains a statue" do
@@ -30,7 +31,7 @@ RSpec.describe Medusa do
     victim = Person.new("Perseus")
 
     medusa.stare(victim)
-
+  
     expect(medusa.statues.count).to eq(1)
     expect(medusa.statues.first.name).to eq("Perseus")
   end
