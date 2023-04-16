c7b26199df98c2e4635b1ae660fb46844a42dd45
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-16 09:27:38 -0600

    Finish practice array within a hash

--- lessons/nested_collections/Examples/array_within_hash.rb
+++ lessons/nested_collections/Examples/array_within_hash.rb
@@ -3,8 +3,12 @@ pizza_toppings = {
   protein: ["pepperoni", "sausage", "sardines"],
   fruit: ["pineapple"]
 }
-
+require 'pry'; binding.pry
 # 1. what is pizza_toppings.count
+# => 3
 # 2. what is pizza_toppings.values
+# => ["green peppers", "jalapeño", "mushrooms], []"pepperoni", "sausage", "sardines"], []"pineapple"]
 # 3. how can I access the element “pineapple”
+# pizza_toppings[:fruit][0]
 # 4. how can I add the element “olives” to the key “veggies”?
+# pizza_toppings[:veggies] << "olives"
