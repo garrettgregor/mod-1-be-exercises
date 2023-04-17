1d2c678f89f171f7f31d8aafbf3e7bc43380809b
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 08:15:43 -0600

    Curator has a way to add photographs to their collection

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -8,4 +8,31 @@ RSpec.describe Curator do
       expect(curator.photographs).to eq([])
     end
   end
+  
+  describe "#add_photograph" do
+    it "can add photographs to the curators collection" do
+      curator = Curator.new
+      
+      expect(curator.photographs).to eq([])
+      
+      photo_1 = Photograph.new({
+        id: "1",      
+        name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+        artist_id: "1",      
+        year: "1954"      
+      })        
+    
+      photo_2 = Photograph.new({
+          id: "2",      
+          name: "Moonrise, Hernandez",      
+          artist_id: "2",      
+          year: "1941"      
+      })        
+      
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+        
+      expect(curator.photographs).to eq([photo_1, photo_2])
+    end
+  end
 end
