be5642019160a9e9e1fd44c15d4906e19ac1c2e5
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 14:22:32 -0600

    WIP on in class event manager practice

--- lessons/csv_files/event_manager/lib/event_manager.rb
+++ lessons/csv_files/event_manager/lib/event_manager.rb
@@ -1,3 +1,26 @@
 # ./lib/event_manager.rb
 require "csv"
 puts "EventManager initialized."
+
+def clean_zip(zipcode)
+  if zipcode.nil?
+    "00000"
+  elsif zipcode.length == 5
+    zipcode
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  elsif zipcode.length < 5
+    zipcode.rjust(5, "0")
+  end
+end
+
+contents = CSV.open("data/event_attendees.csv", headers: true, header_converters: :symbol)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  
+  formatted_zip = clean_zip(zipcode)
+
+  puts "#{name} #{formatted_zip}"
+end
