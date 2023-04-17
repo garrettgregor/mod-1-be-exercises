a586f0835ad3369072d962a5d0f6fea0931e7759
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 07:58:17 -0600

    Begin practice ic museo and initialize with attributes

---
+++ practice_independent_challenges/week5/museo/spec/photograph_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+RSpec.describe Photograph do
+  describe "#initialize" do
+    it "initializes with attributes" do
+      attributes = {
+          id: "1",
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",
+          artist_id: "4",
+          year: "1954"
+        }
+      photograph = Photograph.new(attributes)
+      expect(photograph.id).to eq("1")
+      expect(photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
+      expect(photograph.artist_id).to eq("4")
+      expect(photograph.year).to eq("1954")
+    end
+  end
+end
