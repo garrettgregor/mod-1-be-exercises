d75c43db07996f9191b105c704c638d42a876169
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 08:08:24 -0600

    Calculate age at death of artist

--- practice_independent_challenges/week5/museo/spec/artist_spec.rb
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe Artist do
       expect(artist.country).to eq("United States")
     end
   end
+  
+  describe "#age_at_deat" do
+    it "calculates the age at death" do
+      attributes = {
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      }
+      artist = Artist.new(attributes)
+      expect(artist.born).to eq("1902")
+      expect(artist.died).to eq("1984")
+      expect(artist.age_at_death).to eq(82)
+    end
+  end
 end
