0787d0fb2e65cb973a45d2094e503ac1c979a6ed
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-17 08:19:41 -0600

    Add collection of artists to the curator

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -35,4 +35,31 @@ RSpec.describe Curator do
       expect(curator.photographs).to eq([photo_1, photo_2])
     end
   end
+  
+  describe "#artists" do
+    it "can get a list of the artists in the collection" do
+      curator = Curator.new
+      
+      expect(curator.artists).to eq([])
+      
+      # photo_1 = Photograph.new({
+      #   id: "1",      
+      #   name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+      #   artist_id: "1",      
+      #   year: "1954"      
+      # })        
+    
+      # photo_2 = Photograph.new({
+      #     id: "2",      
+      #     name: "Moonrise, Hernandez",      
+      #     artist_id: "2",      
+      #     year: "1941"      
+      # })        
+      
+      # curator.add_photograph(photo_1)
+      # curator.add_photograph(photo_2)
+        
+      # expect(curator.photographs).to eq([photo_1, photo_2])
+    end
+  end
 end
