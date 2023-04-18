68f10125cb3553f97a60334b374cd02b3eb42437
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-18 13:32:44 -0600

    IC: add method to add activity to reunion list of activities

--- practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/reunion_spec.rb
@@ -2,11 +2,24 @@ require "spec_helper"
 
 RSpec.describe Reunion do
   describe "#initialize" do
-    it "exist" do
+    it "exist with a name and activities" do
       reunion = Reunion.new("1406 BE")
 
       expect(reunion.name).to eq("1406 BE")
       expect(reunion.activities).to eq([])
+    end
+  end
+  
+  describe "#add_activity" do
+    it "exist" do
+      reunion = Reunion.new("1406 BE")
+      activity_1 = Activity.new("Brunch")
+      # => #<Activity:0x007fe4ca1d9438 ...>
+
+      reunion.add_activity(activity_1)
+
+      expect(activity_1).to be_an Activity
+      expect(reunion.activities).to eq([activity_1])
     end
   end
 end
