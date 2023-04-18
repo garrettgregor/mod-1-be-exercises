7ae79c7b818e4f271677b65801f9fd553987626c
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-18 13:16:13 -0600

    IC: Add split to activity

--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -43,4 +43,15 @@ RSpec.describe Activity do
       expect(activity.total_cost).to eq(60)
     end
   end
+  
+  describe "#split" do
+    it "can split the cost amongst all participants" do
+      activity = Activity.new("Brunch")
+      activity.add_participant("Maria", 20)
+      activity.add_participant("Luther", 40)
+      
+      expect(activity.total_cost).to eq(60)
+      expect(activity.split).to eq(30)
+    end
+  end
 end
