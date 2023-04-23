7550648a2bc79bac1809e231cf5f2b3a4c2172e9
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-23 08:37:57 -0600

    Feature: #initialize for Artist with tests

---
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -0,0 +1,26 @@
+
+require "spec_helper"
+
+RSpec.describe Photograph do
+  describe "#initialize" do
+    it "intializes with id, name, born, died and country" do
+      attributes = {
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      }
+
+      artist = Artist.new(attributes)
+
+      expect(artist).to be_an(Artist)
+      expect(artist.id).to eq("2")
+      expect(artist.name).to eq("Ansel Adams")
+      expect(artist.born).to eq("1902")
+      expect(artist.died).to eq("1984")
+      expect(artist.country).to eq("United States")
+      # expect(artist.age_at_death).to eq(82)
+    end
+  end
+end
