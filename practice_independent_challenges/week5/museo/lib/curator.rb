92e63120a29733e7d37b6d3b064b5e6e411df157
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-24 21:56:00 -0600

    Feature: #find_artists_with_multiple_photos

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -21,6 +21,18 @@ class Curator
     end
   end
 
+  def find_artist_name_by_id(artist_id)
+    artist_name = @artists.find do |artist|
+      artist.name if artist.id == artist_id
+    end
+
+    artist_name.name
+  end
+
+  # def all_artist_photos
+  #   @artists
+  # end
+
   def find_photos_by_artist(artist_name)
     search_id = ""
 
@@ -34,5 +46,29 @@ class Curator
       photo.artist_id == search_id
     end
 
+  end
+
+  def find_artists_with_multiple_photos
+    artist_photos = Hash.new(0)
+
+    @photographs.each do |photo|
+      artist_photos[photo.artist_id] += 1
+    end
+
+    artists_with_multiple_photos_objs = []
+
+    artist_photos.each do |key, value|
+      if value > 1
+        artists_with_multiple_photos_objs << find_artist_by_id(key)
+      end
+    end
+
+    artists_with_multiple_photos = []
+
+    artists_with_multiple_photos_objs.each do |artist|
+      artists_with_multiple_photos << artist.name
+    end
+
+    artists_with_multiple_photos
   end
 end
--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -275,6 +275,75 @@ RSpec.describe Curator do
     end
   end
 
+  describe "#find_artist_name_by_id" do
+    it "can find artist name by id" do
+      curator = Curator.new
+      photo_1 = Photograph.new({
+          id: "1",
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",
+          artist_id: "1",
+          year: "1954"
+      })
+
+      photo_2 = Photograph.new({
+          id: "2",
+          name: "Moonrise, Hernandez",
+          artist_id: "2",
+          year: "1941"
+      })
+
+      photo_3 = Photograph.new({
+        id: "3",
+        name: "Identical Twins, Roselle, New Jersey",
+        artist_id: "3",
+        year: "1962"
+      })
+
+      photo_4 = Photograph.new({
+        id: "4",
+        name: "Child with Toy Hand Grenade in Central Park",
+        artist_id: "3",
+        year: "1962"
+      })
+
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+      curator.add_photograph(photo_3)
+      curator.add_photograph(photo_4)
+
+      artist_1 = Artist.new({
+          id: "1",
+          name: "Henri Cartier-Bresson",
+          born: "1908",
+          died: "2004",
+          country: "France"
+      })
+
+      artist_2 = Artist.new({
+          id: "2",
+          name: "Ansel Adams",
+          born: "1902",
+          died: "1984",
+          country: "United States"
+      })
+
+      artist_3 = Artist.new({
+          id: "3",
+          name: "Diane Arbus",
+          born: "1923",
+          died: "1971",
+          country: "United States"
+      })
+
+      curator.add_artist(artist_1)
+      curator.add_artist(artist_2)
+      curator.add_artist(artist_3)
+
+      expect(curator.find_artist_name_by_id("1")).to eq("Henri Cartier-Bresson")
+      expect(curator.find_artist_name_by_id("2")).to eq("Ansel Adams")
+      expect(curator.find_artist_name_by_id("3")).to eq("Diane Arbus")
+    end
+  end
 
   describe "#find_artists_with_multiple_photos" do
     it "can find artists that have more than one" do
