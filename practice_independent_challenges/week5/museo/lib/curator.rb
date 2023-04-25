a08b2db5b71bc1d300459119a22f5f06480aa0e2
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-24 21:23:59 -0600

    Feature: #find_photos_by_artist(name) with tests

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -20,4 +20,19 @@ class Curator
       artist.id == artist_id
     end
   end
+
+  def find_photos_by_artist(artist_name)
+    search_id = ""
+
+    @artists.each do |artist|
+      if artist_name == artist.name
+        search_id = artist.id
+      end
+    end
+
+    @photographs.find_all do |photo|
+      photo.artist_id == search_id
+    end
+
+  end
 end
--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -181,4 +181,97 @@ RSpec.describe Curator do
       expect(curator.find_artist_by_id("1")).to eq(artist_1)
     end
   end
+  describe "#find_artist_by_id" do
+    it "can find artists in collection by id" do
+      curator = Curator.new
+
+      expect(curator.photographs).to eq([])
+
+      photo_1 = Photograph.new({
+          id: "1",
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",
+          artist_id: "1",
+          year: "1954"
+      })
+
+      photo_2 = Photograph.new({
+          id: "2",
+          name: "Moonrise, Hernandez",
+          artist_id: "2",
+          year: "1941"
+      })
+
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+
+      expect(curator.artists).to eq([])
+
+      artist_1 = Artist.new({
+          id: "1",
+          name: "Henri Cartier-Bresson",
+          born: "1908",
+          died: "2004",
+          country: "France"
+      })
+
+      artist_2 = Artist.new({
+          id: "2",
+          name: "Ansel Adams",
+          born: "1902",
+          died: "1984",
+          country: "United States"
+      })
+
+      curator.add_artist(artist_1)
+      curator.add_artist(artist_2)
+
+      expect(curator.artists).to eq([artist_1, artist_2])
+
+      expect(curator.find_artist_by_id("1")).to eq(artist_1)
+    end
+  end
+
+  describe "#find_photos_by_artist" do
+    it "can find artist's photos" do
+      curator = Curator.new
+      photo_1 = Photograph.new({
+          id: "1",
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",
+          artist_id: "1",
+          year: "1954"
+      })
+
+      photo_2 = Photograph.new({
+          id: "2",
+          name: "Moonrise, Hernandez",
+          artist_id: "2",
+          year: "1941"
+      })
+
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+
+      artist_1 = Artist.new({
+          id: "1",
+          name: "Henri Cartier-Bresson",
+          born: "1908",
+          died: "2004",
+          country: "France"
+      })
+
+      artist_2 = Artist.new({
+          id: "2",
+          name: "Ansel Adams",
+          born: "1902",
+          died: "1984",
+          country: "United States"
+      })
+
+      curator.add_artist(artist_1)
+      curator.add_artist(artist_2)
+
+      expect(curator.find_photos_by_artist("Henri Cartier-Bresson")).to eq([photo_1])
+      expect(curator.find_photos_by_artist("Ansel Adams")).to eq([photo_2])
+    end
+  end
 end
