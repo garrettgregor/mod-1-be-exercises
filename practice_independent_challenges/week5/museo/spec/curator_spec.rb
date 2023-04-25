7c2625e5369015ce7ddd3d4db276063c50ba9133
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-24 21:25:28 -0600

    Test: #find_artists_with_multiple_photos

--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -274,4 +274,76 @@ RSpec.describe Curator do
       expect(curator.find_photos_by_artist("Ansel Adams")).to eq([photo_2])
     end
   end
+
+
+  describe "#find_artists_with_multiple_photos" do
+    it "can find artists that have more than one" do
+      curator = Curator.new
+      photo_1 = Photograph.new({
+          id: "1",
+          name: "Rue Mouffetard, Paris (Boy with Bottles)",
+          artist_id: "1",
+          year: "1954"
+      })
+
+      photo_2 = Photograph.new({
+          id: "2",
+          name: "Moonrise, Hernandez",
+          artist_id: "2",
+          year: "1941"
+      })
+
+      photo_3 = Photograph.new({
+        id: "3",
+        name: "Identical Twins, Roselle, New Jersey",
+        artist_id: "3",
+        year: "1962"
+      })
+
+      photo_4 = Photograph.new({
+        id: "4",
+        name: "Child with Toy Hand Grenade in Central Park",
+        artist_id: "3",
+        year: "1962"
+      })
+
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+      curator.add_photograph(photo_3)
+      curator.add_photograph(photo_4)
+
+      artist_1 = Artist.new({
+          id: "1",
+          name: "Henri Cartier-Bresson",
+          born: "1908",
+          died: "2004",
+          country: "France"
+      })
+
+      artist_2 = Artist.new({
+          id: "2",
+          name: "Ansel Adams",
+          born: "1902",
+          died: "1984",
+          country: "United States"
+      })
+
+      artist_3 = Artist.new({
+          id: "3",
+          name: "Diane Arbus",
+          born: "1923",
+          died: "1971",
+          country: "United States"
+      })
+
+      curator.add_artist(artist_1)
+      curator.add_artist(artist_2)
+      curator.add_artist(artist_3)
+
+      expect(curator.find_photos_by_artist("Henri Cartier-Bresson")).to eq([photo_1])
+      expect(curator.find_photos_by_artist("Ansel Adams")).to eq([photo_2])
+      expect(curator.find_photos_by_artist("Diane Arbus")).to eq([photo_3, photo_4])
+      expect(curator.find_artists_with_multiple_photos).to eq(["Diane Arbus"])
+    end
+  end
 end
